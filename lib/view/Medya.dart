e1637451ca3ea497ef8af12ad4cb1a89a9490ee0
simurgonur <simurgonur@gmail.com>
2022-11-29 01:28:01 +0300

    floating

--- lib/view/Medya.dart
+++ lib/view/Medya.dart
@@ -1,23 +1,21 @@
 import 'dart:io' as ioo;
 
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:loko_media/database/AlbumDataBase.dart';
 import 'package:loko_media/models/Album.dart';
 import 'package:loko_media/view_model/layout.dart';
 import 'package:loko_media/view_model/media_view_model.dart';
-
-import 'PhotoViewer.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 
 class Medya extends StatefulWidget {
   int? id;
-  // final PageController pageController;
-  // final int index;
+  final PageController pageController;
+  late int index;
 
-  Medya({
-    this.id,
-    /*this.index = 0*/
-  });
-  // : pageController = PageController(initialPage: index);
+  Medya({this.id, this.index = 0})
+      : pageController = PageController(initialPage: index);
 
   @override
   State<Medya> createState() => MedyaState();
@@ -25,7 +23,7 @@ class Medya extends StatefulWidget {
 
 class MedyaState extends State<Medya> {
   List<Medias> fileList = [];
-
+  late int index = widget.index;
   List<int> selectedMedias = [];
   bool selectionMode = false;
 
@@ -86,9 +84,8 @@ class MedyaState extends State<Medya> {
   mediaCardBuilder(Medias) {
     Widget? media;
     bool secilimi = selectedMedias.indexOf(Medias.id) == -1 ? false : true;
-    var secimIcon = secilimi == true
-        ? Icons.check_box_outlined
-        : Icons.check_box_outline_blank;
+
+    var secimIcon = secilimi == true ? Icons.check_circle : null;
     double margin = secilimi == true ? 8 : 4;
     switch (Medias.fileType) {
       case 'image':
@@ -117,7 +114,7 @@ class MedyaState extends State<Medya> {
           switch (Medias.fileType) {
             case 'image':
               {
-                openGallery(Medias.path.toString(), Medias.name);
+                openGallery();
                 break;
               }
           }
@@ -129,22 +126,27 @@ class MedyaState extends State<Medya> {
       child: Container(
         margin: EdgeInsets.all(margin),
         child: Stack(
+          fit: StackFit.expand,
           children: [
             media!,
-            Padding(
-              padding: const EdgeInsets.all(2),
-              child: Material(
-                color: Colors.transparent,
-                child: Ink(
-                  height: 32,
-                  width: 32,
-                  child: IconButton(
-                    iconSize: 16,
-                    icon: Icon(secimIcon, color: Color(0xff017eba)),
-                    color: Colors.white,
-                    onPressed: () {
-                      selectMedia(Medias);
-                    },
+            Positioned(
+              left: 1,
+              top: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(2),
+                child: Material(
+                  color: Colors.transparent,
+                  child: Ink(
+                    height: 10,
+                    width: 10,
+                    child: IconButton(
+                      iconSize: 16,
+                      icon: Icon(secimIcon, color: Colors.white),
+                      color: Colors.white,
+                      onPressed: () {
+                        selectMedia(Medias);
+                      },
+                    ),
                   ),
                 ),
               ),
@@ -158,6 +160,38 @@ class MedyaState extends State<Medya> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        backgroundColor: Color(0xff017eba),
+        activeBackgroundColor: Colors.red,
+        activeForegroundColor: Colors.white,
+        closeManually: true,
+        children: [
+          SpeedDialChild(
+            child: Icon(Icons.check),
+            onTap: () {},
+            label: 'Hepsini Seç',
+            backgroundColor: Color(0xff26334d),
+            labelBackgroundColor: Color(0xff26334d),
+          ),
+          SpeedDialChild(
+              onTap: () {},
+              child: Icon(Icons.map),
+              label: 'Seçilenleri Haritalandır'),
+          SpeedDialChild(
+              onTap: () {},
+              child: Icon(Icons.delete),
+              label: 'Seçilenleri Sil'),
+          SpeedDialChild(
+              onTap: () {},
+              child: Icon(Icons.cancel),
+              label: 'Seçimi İptal Et'),
+          SpeedDialChild(
+              onTap: () {},
+              child: Icon(Icons.share),
+              label: 'Seçilenleri Paylaş'),
+        ],
+      ),
       body: GridView.builder(
         itemCount: fileList.length,
         padding: const EdgeInsets.symmetric(
@@ -167,8 +201,6 @@ class MedyaState extends State<Medya> {
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           mainAxisExtent: context.dynamicWidth(4),
           maxCrossAxisExtent: context.dynamicHeight(8),
-          mainAxisSpacing: 4,
-          crossAxisSpacing: 4,
         ),
         itemBuilder: (BuildContext context, int index) {
           return mediaCardBuilder(fileList[index]);
@@ -177,10 +209,45 @@ class MedyaState extends State<Medya> {
     );
   }
 
-  openGallery(path, name) {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => PhotoViewer(imagePath: path, name: name)));
+  openGallery() {
+    return Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => Stack(alignment: Alignment.bottomLeft, children: [
+              PhotoViewGallery.builder(
+                pageController: widget.pageController,
+                itemCount: fileList.length,
+                builder: (BuildContext context, int index) {
+                  return PhotoViewGalleryPageOptions(
+                      imageProvider: FileImage(
+                        ioo.File(fileList[index].path.toString()),
+                      ),
+                      minScale: PhotoViewComputedScale.contained * 0.8,
+                      maxScale: PhotoViewComputedScale.covered * 2);
+                },
+                onPageChanged: (index) => setState(() {
+                  this.index = index;
+                }),
+                scrollPhysics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                enableRotation: true,
+                backgroundDecoration: BoxDecoration(
+                    color: Theme.of(context).scaffoldBackgroundColor),
+                loadingBuilder: (context, event) => Center(
+                  child: Container(
+                    width: 30.0,
+                    height: 30.0,
+                    child: CircularProgressIndicator(color: Colors.white),
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 16, left: 8),
+                  child: Text(
+                    'Resim ${index + 1}/${fileList.length.toString()}',
+                    style: TextStyle(),
+                  ),
+                ),
+              )
+            ])));
   }
 }
